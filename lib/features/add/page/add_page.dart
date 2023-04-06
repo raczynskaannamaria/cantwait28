3d4c5e9716bef7a7fef1b02bed9a73a76ba9b72a
Anna Raczyńska <raczynska.annamaria@gmail.com>
2023-04-06 17:21:41 +0200

    Show SnackBar on adding error

--- lib/features/add/page/add_page.dart
+++ lib/features/add/page/add_page.dart
@@ -25,6 +25,14 @@ class _AddPageState extends State<AddPage> {
           if (state.saved) {
             Navigator.of(context).pop();
           }
+          if (state.errorMessage.isNotEmpty) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(state.errorMessage),
+                backgroundColor: Colors.redAccent,
+              ),
+            );
+          }
         },
         child: BlocBuilder<AddCubit, AddState>(
           builder: (context, state) {
